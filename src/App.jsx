a282f6c81251d060f487379a52b7a217528210a1
richardyzx91 <richardyan422@gmail.com>
2024-04-06 21:02:45 -0400

    test fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+// import { ApiKeySession, ProfilesApi, EventsApi } from 'klaviyo-api';
 import './App.css';
 import SearchIcon from './search.svg';
 import SearchResults from './SearchResults';
 import CryptoDatabase from './CryptoDatabase.json';
 
 const App = () => {
-  const [results, setResults] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
   const data = CryptoDatabase.data;
+
+  const klaviyoApiBaseUrl = "https://a.klaviyo.com/api"
+  const klaviyoPk = "pk_cbc2d482a4606854dc0f7b29ef1b959bd3"
+
+  // const klaviyoSession = new ApiKeySession(klaviyoPk)
+  // const eventsAPI = new EventsApi(klaviyoSession)
+  // const profilesApi = new ProfilesApi(klaviyoSession)
+
+  // try {
+  //   const testOutput = profilesApi.getProfiles().then(r => console.log(`Test Output: ${r}`))
+  // } catch (e) {
+  //   console.log('Error: ' + e)
+  // }
+  
+  // const testOutput = eventsAPI.getEvent("57VXXrihXF6").then()
+  // console.log(`Test Output: ${testOutput}`)
+
+
+  // const [results, setResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+  const [testOutput, setTestOutput] = useState('None')
+
+  useEffect(() => {
+    klaviyoGetProfiles()
+  }, [])
+
+  const klaviyoGetProfiles = async () => {
+    const requestUrl = `${klaviyoApiBaseUrl}/profiles/?page[size]=20`
+    
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        revision: '2024-02-15',
+        Authorization: `Klaviyo-API-Key ${klaviyoPk}`
+      },
+      redirect: "follow"
+    }
+
+    fetch(requestUrl, requestOptions)
+    .then(res => setTestOutput(res.toString()))
+    .catch(err => console.error('error:' + err));
+  }
 
   return (
     <div className="app">
       <h1>See Supported Cryptos</h1>
+      {testOutput}
 
       <div className="search">
         <input 
